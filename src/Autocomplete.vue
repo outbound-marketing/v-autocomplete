22ae5a30ee0277e99a1806da1576040465c9590d
ArthurVilaca <vilaca.arthur.f@gmail.com>
2020-01-23 09:36:22 -0300

    alterando local do evento

--- src/Autocomplete.vue
+++ src/Autocomplete.vue
@@ -10,9 +10,9 @@
             @keydown.up="keyUp" @keydown.down="keyDown">
     </div>
     <div class="v-autocomplete-list" v-if="show">
-      <div class="v-autocomplete-list-item" v-for="item, i in internalItems" @click.native="onClickItem(item)"
+      <div class="v-autocomplete-list-item" v-for="item, i in internalItems"
            :class="{'v-autocomplete-item-active': i === cursor}" @mouseover="cursor = i">
-        <div :is="componentItem" :item="item" :searchText="searchText"></div>
+        <div :is="componentItem" :item="item" :searchText="searchText" @click="onClickItem(item)"></div>
       </div>
     </div>
   </div>
@@ -81,6 +81,7 @@ export default {
     },
 
     onClickItem(item) {
+      console.log('evento')
       this.onSelectItem(item)
       this.$emit('item-clicked', item)
     },
